684a5a32c3c3d865f617cae4b94579161ebd60b9
Stiliyan <stiliyan_dragnev@yahoo.com>
2021-07-18 18:31:29 +0300

    Add public names to datapreparation

---
+++ nic/datapreparation/__init__.py
@@ -0,0 +1,11 @@
+from nic.datapreparation.downloading import (
+    download_mscoco,
+    split_out_test_data,
+)
+from nic.datapreparation.preprocessing import (
+    ImageOptions,
+    MetaTokens,
+    preprocess_captions,
+    preprocess_data,
+    preprocess_images,
+)
