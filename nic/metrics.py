cbff19c1159b5b133c28431615d9293050647ccb
Stiliyan <stiliyan_dragnev@yahoo.com>
2021-08-01 17:55:34 +0300

    Add the metrics module

---
+++ nic/metrics.py
@@ -0,0 +1,50 @@
+from nltk.translate.bleu_score import corpus_bleu
+import tensorflow as tf
+from tensorflow import keras
+
+
+def create_loss_object():
+    return keras.losses.SparseCategoricalCrossentropy(
+        from_logits=True
+    )
+
+
+class Perplexity(keras.metrics.Metric):
+    def __init__(self, name="perplexity", **kwargs):
+        super().__init__(name=name, **kwargs)
+        self._cross_entropy = self.add_weight(
+            name="cross-entropy",
+            initializer="zeros",
+            dtype=self.dtype,
+            trainable=False,
+        )
+        self._total_target_words = self.add_weight(
+            name="total-target-words",
+            initializer="zeros",
+            dtype=tf.int32,
+            trainable=False,
+        )
+
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        target_words = tf.math.count_nonzero(
+            y_true,
+            dtype=self._total_target_words.dtype
+        )
+        self._total_target_words.assign_add(target_words)
+
+        cross_entropy = create_loss_object()
+        loss = cross_entropy(y_true, y_pred)
+        self._cross_entropy.assign_add(
+            target_words * loss
+        )
+
+    def result(self):
+        return tf.exp(self._cross_entropy / self._total_target_words)
+
+    def reset_states(self):
+        self._total_target_words.assign(0)
+        self._cross_entropy.assign(0.0)
+
+
+def bleu_score_of(translated_sentences, reference_sentences):
+    return 100 * corpus_bleu(reference_sentences, translated_sentences)
