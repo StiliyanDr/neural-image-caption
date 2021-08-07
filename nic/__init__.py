23721f3a956f38b0b7f67f97bc393dc04211f372
Stiliyan <stiliyan_dragnev@yahoo.com>
2021-08-07 18:15:01 +0300

    Define the package's public names

---
+++ nic/__init__.py
@@ -0,0 +1,13 @@
+from nic import datapreparation as dp
+from nic.model import (
+    connect,
+    define_decoder_model,
+    define_encoder_model,
+    define_model,
+    RNNOptions,
+)
+from nic.training import (
+    compile_model,
+    restore_model,
+    train_model,
+)
